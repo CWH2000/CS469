902b0f3cf4c66a1a0b6b5718dc88c248cfe1013d
CWH2000 <71786867+CWH2000@users.noreply.github.com>
2023-03-21 14:00:18 -0700

    Add files via upload

---
+++ data/assignment_progress.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.assignment_progress (user_id,assignment_id,progress,completed,date_completed,grade) VALUES
+	 (2,115,NULL,NULL,'2023-03-18 00:00:00',85.0),
+	 (2,118,NULL,NULL,'2023-03-17 00:00:00',92.0),
+	 (1,120,NULL,NULL,'2023-03-16 00:00:00',78.0),
+	 (3,119,NULL,NULL,'2023-03-15 00:00:00',90.0),
+	 (3,121,NULL,NULL,'2023-03-14 00:00:00',88.0),
+	 (1,122,NULL,NULL,'2023-03-13 00:00:00',95.0);
