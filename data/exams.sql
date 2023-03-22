e94e3d0b00deb6dc9dd74ae57efcc9d8c114a0c4
CWH2000 <71786867+CWH2000@users.noreply.github.com>
2023-03-21 21:10:29 -0700

    Add files via upload

---
+++ data/exams.sql
@@ -0,0 +1,83 @@
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (1,114,'Midterm 1','2023-06-10','10:00:00',120,'http://example.com/exam1'),
+	 (2,114,'Midterm 2','2023-07-20','14:00:00',120,'http://example.com/exam2'),
+	 (3,114,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (4,115,'Exam 1','2023-06-10','13:00:00',90,'http://example.com/exam1'),
+	 (5,115,'Exam 2','2023-07-20','10:00:00',90,'http://example.com/exam2'),
+	 (6,115,'Final','2023-08-30','14:00:00',180,'http://example.com/final'),
+	 (7,116,'Midterm 1','2023-06-10','14:00:00',120,'http://example.com/exam1'),
+	 (8,116,'Midterm 2','2023-07-20','10:00:00',120,'http://example.com/exam2'),
+	 (9,116,'Final','2023-08-30','13:00:00',180,'http://example.com/final'),
+	 (10,117,'Exam 1','2023-06-10','10:00:00',90,'http://example.com/exam1');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (11,117,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (12,117,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (13,118,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (14,118,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (15,118,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (16,119,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (17,119,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (18,119,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (19,120,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (20,120,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (21,120,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (22,121,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (23,121,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (24,121,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (25,122,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (26,122,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (27,122,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (28,123,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (29,123,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (30,123,'Final','2023-08-30','10:00:00',180,'http://example.com/final');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (31,124,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (32,124,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (33,124,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (34,125,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (35,125,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (36,125,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (37,126,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (38,126,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (39,126,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (40,127,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (41,127,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (42,127,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (43,128,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (44,128,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (45,128,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (46,129,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (47,129,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (48,129,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (49,130,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (50,130,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (51,130,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (52,131,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (53,131,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (54,131,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (55,132,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (56,132,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (57,132,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (58,133,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (59,133,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (60,133,'Final','2023-08-30','10:00:00',180,'http://example.com/final');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (61,134,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (62,134,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (63,134,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (64,135,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (65,135,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (66,135,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (67,136,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (68,136,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (69,136,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (70,137,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1');
+INSERT INTO public.exams (id,course_id,title,"date",exam_time,duration_minutes,link) VALUES
+	 (71,137,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (72,137,'Final','2023-08-30','10:00:00',180,'http://example.com/final'),
+	 (73,138,'Midterm 1','2023-06-10','13:00:00',120,'http://example.com/exam1'),
+	 (74,138,'Exam 2','2023-07-20','13:00:00',90,'http://example.com/exam2'),
+	 (75,138,'Final','2023-08-30','10:00:00',180,'http://example.com/final');
